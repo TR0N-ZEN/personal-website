41ce089c9046fe8983e521de1feaaa86c272188e
Theo <theo.reichert7@gmail.com>
2019-07-05 10:19:19 +0200

    1.0

---
+++ script.js
@@ -0,0 +1,9 @@
+		$(document).ready(function (){
+			$('#IM').hide();
+			$('#right h2').click(function() {
+					$('#IM').slideToggle(300);
+			});
+			$('#close').click(function() {
+				$('#IM').slideToggle(300);
+			});
+		});
