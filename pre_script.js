914b158645811ef8091f63a5091fbff09cff5346
Theo Reichert <theo.reichert7@gmail.com>
2020-01-14 20:17:49 +0100

    14.01.20

---
+++ pre_script.js
@@ -0,0 +1,10 @@
+document.write("<p id='willione'>Hello there I am at the beginning.</p><style>#willione{color: red;}</style>");
+document.getElementById("jscript").innerHTML = "Wrote this with JavaScript";
+document.getElementById("inmath").innerHTML = "Wrote this with JavaScript";
+window.alert(10);
+document.getElementById("head").innerHTML = "<h1>Wrote this with JavaScript and replaced the heading</h1>";
+window.alert(4+5);
+document.getElementById("head").innerHTML = 4 + 5;
+window.alert(4+4);
+document.write("<p id='willi'>Hello there I am at the end.</p><style>#willi{color: red;}</style>");
+document.write("<p id='willione'>Hello there I am at the beginning.</p><style>#willione{color: red;}</style>");
--- script.js
+++
@@ -1,9 +0,0 @@
-		$(document).ready(function (){
-			$('#IM').hide();
-			$('#right h2').click(function() {
-					$('#IM').slideToggle(300);
-			});
-			$('#close').click(function() {
-				$('#IM').slideToggle(300);
-			});
-		});
